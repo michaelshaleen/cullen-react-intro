08314e3c80f3b2e76bf54cf9d84a4faf2fb265f6
Michael Shaleen <shaleenmichael@protonmail.com>
2021-02-15 16:06:34 -0600

    counters and reset works

--- src/App.js
+++ src/App.js
@@ -1,41 +1,51 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//fancy import syntax
+
+// import React from 'react';
+// const useState = React.useState;
+// //
+
+//OBJECT DESTRUCTURING
+import { useState } from 'react';
 //client side (REACT) equivalent of import
 //and assigning to variables
-import HelloCullen from './HelloCullen';
+//import HelloCullen from './HelloCullen';
 
 //APP IS A COMPONENT
 // REACT IS COMPOSED OF COMPONENTS
 //COMPONENTS ARE FUNCTIONS THAT RETURN JSX
 //YOU CAN NEST COMPONENTS
 function App() {
-  let bigName = 'self';
-  let myH1 = <h1>Go home, {bigName}, you're drunk?????</h1>;
+  //wouldn't render
+  //let Count = 0;
+
+  //useState is a 'hook'
+  // tool used inside react components
+  let Name = 'self';
+  //let myH1 = <h1>Go home, {Name}, you're drunk?????</h1>;
+  //setDrinkCount function to change drinkCount
+  //drinkCount is current val of state
+
+  //ARRAY DESTRUCTURING
+  const returnValOfUseState = useState(1);
+  const currentState = returnValOfUseState[0];
+  const setCount = returnValOfUseState[1];
+  const [drinkCount, setDrinkCount] = useState(1);
 
   //"HTML" is actually "JSX"
   //"JAVASCRIPT EXTENDED"
   // AS IN APP.JSX REFERS TO MYH1 SET UP
   return (
-    <div className="App">
-      {myH1}
-      <header className="App-header">
-        {/*Hello cullen is a react component. uppercamel case is convention for
-        components in REACT */}
-        <HelloCullen />
+    <div>
+      <h1>Cullen Training Hour!</h1>
+      <p>
+        {Name} has had {drinkCount} trainings today
+      </p>
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to win.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setDrinkCount(drinkCount + 1)}>Train up</button>
+
+      <button onClick={() => setDrinkCount(0)}>Rest Day</button>
     </div>
   );
 }
